3803ca81dca86395d77e6db5e51f04d9b6f312ff
ak4422920 <ak4422920@gmail.com>
2025-10-29 09:52:21 +0530

    Refactor CHANNELS and improve comments
    Updated CHANNELS list and added comments for clarity.

--- info.py
+++ info.py
@@ -29,7 +29,7 @@ AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002431609970'))
 
 # This Is File Channel Where You Upload Your File Then Bot Automatically Save It In Database 
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002294594986 -1001769437975 -1001855645227 -1002834794495 -1002776067152 -1003170296959 -1003167170347 -1002107295737').split()]  # For Multiple Id Use One Space Between Each.
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002294594986 -1001769437975 -1001855645227 -1002834794495 -1002776067152 -1003170296959 -1003167170347 -1002107295737 -1001964139883').split()]  # For Multiple Id Use One Space Between Each.
 
 # auth_channel means force subscribe channel.
 # if REQUEST_TO_JOIN_MODE is true then force subscribe work like request to join fsub, else if false then work like normal fsub.
@@ -196,6 +196,7 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
